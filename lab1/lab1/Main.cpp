d072c8a10b27b0f18e011c013cc300d8c8dfd373
Yauheni Harkavik <bugatti1701@gmail.com>
2019-02-04 16:02:00 +0300

    Added first lab

---
+++ lab1/lab1/Main.cpp
@@ -0,0 +1,63 @@
+#include <Windows.h>
+#include <iostream>
+#include "Matrix.h"
+using namespace std;
+
+typedef float TYPE;
+
+#define MAIN_WIDTH 200
+#define MAIN_HEIGHT 200
+#define MATRIX_A_CELL_WIDTH 8
+#define MATRIX_A_CELL_HEIGHT 12
+#define MATRIX_B_CELL_WIDTH 12
+#define MATRIX_B_CELL_HEIGHT 8
+
+int main() {
+	Matrix<TYPE> matrixA(MAIN_WIDTH, MAIN_HEIGHT, MATRIX_A_CELL_WIDTH, MATRIX_A_CELL_HEIGHT),
+		matrixB(MAIN_WIDTH, MAIN_HEIGHT, MATRIX_B_CELL_WIDTH, MATRIX_B_CELL_HEIGHT),
+		matrixC, matrixD;
+
+	matrixA.generateValues();
+	matrixB.generateValues();
+
+	ULONGLONG resultTimeSse;
+	ULONGLONG resultTime;
+	ULONGLONG resultTimeNotVectorized;
+
+	ULONGLONG endTime;
+
+	ULONGLONG startTime = GetTickCount64();
+	matrixC = matrixA.multiplyVectorized(matrixB);
+	endTime = GetTickCount64();
+
+	resultTime = endTime - startTime;
+
+	startTime = GetTickCount64();
+	matrixD = matrixA.multiplyNotVectorized(matrixB);
+	endTime = GetTickCount64();
+
+	resultTimeNotVectorized = endTime - startTime;
+
+	startTime = GetTickCount64();
+	matrixD = matrixA.multiplySse(matrixB);
+	endTime = GetTickCount64();
+
+	resultTimeSse = endTime - startTime;
+
+	cout << "Victorization Ticks: " << resultTime << "." << endl;
+	cout << "SSE Ticks: " << resultTimeSse << "." << endl;
+	cout << "Not Victorized Ticks: " << resultTimeNotVectorized << "." << endl << endl;
+
+	cout << "Vectorized speed up: x" << (double)resultTimeNotVectorized / (double)resultTime << "." << endl;
+	cout << "SSE speed up: x" << (double)resultTimeNotVectorized / (double)resultTimeSse << ".\n" << endl;
+
+	if (matrixC.equals(matrixD)) {
+		cout << "Matrices by SSE and by Victorization are equal." << endl;
+	}
+	else {
+		cout << "Matrices by SSE and by Victorization are not equal." << endl;
+	}
+
+	system("pause");
+	return 0;
+}
